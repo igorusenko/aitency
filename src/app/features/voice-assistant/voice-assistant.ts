24d084abfbdd8bbaecf666f655ea52106c4c2c7f
IGOR-COMPONENT\Igor <igorusenko2003@gmail.com>
2026-03-04 17:52:01 +0700

    voice-assistant intents logic added

--- src/app/features/voice-assistant/voice-assistant.ts
+++ src/app/features/voice-assistant/voice-assistant.ts
@@ -33,24 +33,31 @@ export class VoiceAssistant {
   constructor() {
     effect(() => {
       const calendarIndex = this.automationsStore.intents().findIndex((intent: any) => intent.key === 'calendar')
+      if (this.automationsStore.intents()[calendarIndex])
       this.automationsStore.intents()[calendarIndex].active = this.calendarProcessing();
 
       const bookingIndex = this.automationsStore.intents().findIndex((intent: any) => intent.key === 'booking')
+      if (bookingIndex > -1)
       this.automationsStore.intents()[bookingIndex].active = this.bookingProcessing();
 
       const priceInquiryIndex = this.automationsStore.intents().findIndex((intent: any) => intent.key === 'price_inquiry')
+      if (priceInquiryIndex > -1)
       this.automationsStore.intents()[priceInquiryIndex].active = this.priceInquiryProcessing();
 
       const serviceInfoIndex = this.automationsStore.intents().findIndex((intent: any) => intent.key === 'service_info')
+      if (serviceInfoIndex > -1)
       this.automationsStore.intents()[serviceInfoIndex].active = this.serviceInfoProcessing();
 
       const cancelAppointmentIndex = this.automationsStore.intents().findIndex((intent: any) => intent.key === 'cancel_appointment')
+      if (cancelAppointmentIndex > -1)
       this.automationsStore.intents()[cancelAppointmentIndex].active = this.cancelAppointmentProcessing();
 
       const rescheduleAppointmentIndex = this.automationsStore.intents().findIndex((intent: any) => intent.key === 'reschedule_appointment')
+      if (rescheduleAppointmentIndex > -1)
       this.automationsStore.intents()[rescheduleAppointmentIndex].active = this.rescheduleAppointmentProcessing();
 
       const handoffToHumanIndex = this.automationsStore.intents().findIndex((intent: any) => intent.key === 'handoff_to_human')
+      if (handoffToHumanIndex > -1)
       this.automationsStore.intents()[handoffToHumanIndex].active = this.handoffToHumanProcessing();
     });
   }
